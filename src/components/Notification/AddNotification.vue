b8db9302fbaf29504962b5aae0218ae86b1c2ae4
Bharat Agarwal <agarwalbharat68@gmail.com>
2020-03-20 19:33:18 +0530

    add push notifications done

---
+++ src/components/Notification/AddNotification.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="center-text">
+    <v-dialog v-model="dialog" width="800" scrollable>
+      <template v-slot:activator="{ on }">
+        <v-btn v-on="on" class="primary ml-2">Add Notification</v-btn>
+      </template>
+      <v-card>
+        <v-card-title class="headline google-font" primary-title>Add Notification</v-card-title>
+        <v-card-text>
+          <v-container fluid>
+            <v-row align="center">
+              <v-col cols="12" md="6" class="pa-1 ma-0">
+                <v-text-field v-model="dialogData.title" label="Title" type="text" outlined></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6" class="pa-1 ma-0">
+                <v-text-field v-model="dialogData.image" label="Image Link" type="url" outlined></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6" class="pa-1 ma-0 order-2 order-md-1">
+                <v-textarea v-model="dialogData.body" label="Body" type="text" outlined></v-textarea>
+              </v-col>
+              <v-col cols="12" md="6" class="pa-1 ma-0 order-1 order-md-2">
+                <v-img :src="(dialogData.image.length > 10)?dialogData.image:''"></v-img>
+              </v-col>
+              <v-col cols="12" md="6" class="pa-1 ma-0">
+                <v-text-field v-model="dialogData.regLink" label="Reg Link" type="text" outlined></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6" class="pa-1 ma-0">
+                <v-text-field v-model="dialogData.eventID" label="Event ID" type="text" outlined></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="dialog = false">Close</v-btn>
+          <v-btn
+            color="green"
+            text
+            :loading="isAdding"
+            @click="addNoti"
+          >Add</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import firebase from '@/config/firebase';
+
+export default {
+  name: "addNotification",
+  data: () => ({
+    dialog: false,
+    isAdding:false,
+    dialogData:{
+        title:"",
+        body:"",
+        image:"",
+        regLink:"",
+        eventID:""
+    }
+  }),
+  methods: {
+    addNoti() {
+        this.isAdding = true;
+        var data ={
+            title:this.dialogData.title,
+            body:this.dialogData.body,
+            image:this.dialogData.image,
+            regLink:this.dialogData.regLink,
+            eventID:this.dialogData.eventID,
+            noTimeSend:0,
+            createdOn:new Date(),
+            sentTime:"",
+        }
+        firebase.firestore.collection("pushNotifications").add(data).then(res=>{
+            console.log(res);
+            this.isAdding = false;
+            this.$emit("addedSuccess", true);
+            this.dialog = false;
+        })
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/Notifications.vue
+++ src/views/Notifications.vue
@@ -1,14 +1,63 @@
 <template>
-  <div>
-    <h1>Notification</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa fugiat aut, sunt dolorem quas earum dicta eum unde nemo assumenda nostrum ex quos. Sed corporis aut repellat quibusdam animi ut.</p>
-  </div>
+  <v-container fluid>
+    <Snakebar
+      :message="snakeBarMessage"
+      :isShow.sync="isSnakeBarVisible"
+      :color="snakeBarColor"
+      :timeout="snakeBarTimeOut"
+    />
+    <v-row justify="center" align="center">
+        <v-col cols="12" md="11">
+          <v-toolbar class="elevation-1">
+            <v-toolbar-title class="google-font">PushNotifications Details:</v-toolbar-title>
+            <div class="flex-grow-1"></div>
+            <v-text-field
+              flat
+              v-model="search"
+              solo-inverted
+              hide-details
+              prepend-inner-icon="mdi-search-web"
+              label="Search"
+              single-line
+              class="hidden-sm-and-down google-font"
+            ></v-text-field>
+            <addNotification @addedSuccess="addedd" />
+          </v-toolbar>
+        </v-col>
+      </v-row>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, illo qui. Neque maiores voluptatibus rerum magni vel tempore molestiae reprehenderit vitae delectus, non, fugiat sit reiciendis earum, quia assumenda tenetur?</h1>
+  </v-container>
 </template>
 
 <script>
-export default {
+import Snakebar from "@/components/Common/Snakebar";
+import AddNotification from '@/components/Notification/AddNotification';
+
+
+export default {  
   name: "Notifications",
-  data:()=>({})
+  components: {
+    Snakebar,
+    AddNotification
+  },
+  data: () => ({
+    isSearch: false,
+    search: "",
+    snakeBarMessage: "",
+    isSnakeBarVisible: false,
+    snakeBarColor: "green",
+    snakeBarTimeOut: 5000,
+    isLoading: false
+  }),
+  methods:{
+    addedd(e){
+      if(e=='true' || e==true){
+        this.snakeBarMessage = "Added Success";
+        this.isSnakeBarVisible = true;
+      //  this.loadData();
+      }
+    }
+  }
 };
 </script>
 
