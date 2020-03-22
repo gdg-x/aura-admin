d6bce53aae3bcd483167a3e1d5b0e962ffcae5e5
Vrijraj Singh <vrijraj2396@gmail.com>
2020-03-22 12:20:12 +0530

    Home page updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,52 @@
 <template>
   <v-container style="max-width:1600px">
     <v-row>
-      <v-col>
+      <!-- <v-col>
         <p>{{userEmail}}</p>
         <v-btn class="primary" v-on:click="logout">Logout</v-btn>
+      </v-col> -->
+      <v-col md="3" class="">
+        <v-card
+        >
+          <v-list-item>
+            <v-list-item-avatar color="grey"></v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title class="headline">Our Changing Planet</v-list-item-title>
+              <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
+            height="194"
+          ></v-img>
+
+          <v-card-text>
+            Visit ten places on our planet that are undergoing the biggest changes today.
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              text
+              color="deep-purple accent-4"
+            >
+              Read
+            </v-btn>
+            <v-btn
+              text
+              color="deep-purple accent-4"
+            >
+              Bookmark
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>mdi-share-variant</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
