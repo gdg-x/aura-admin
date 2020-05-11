4f99e086001709662fba19d7f165d6d444571b38
Bharat Agarwal <agarwalbharat68@gmail.com>
2020-05-11 14:14:32 +0530

    index.js updated

--- functions/modules/team.js
+++ functions/modules/team.js
@@ -6,20 +6,20 @@ const APP_NAME = 'Aura Admin';
 const mailTransport = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: functions.config().someservice.email,
-      pass: functions.config().someservice.password
+        user: functions.config().someservice.email,
+        pass: functions.config().someservice.password
     },
 });
 
 exports.addMessage = functions.https.onCall((data) => {
     console.log(data)
-    return data.text+ ' Vrijraj Singh'
+    return data.text + ' Vrijraj Singh'
 });
 
-exports.disabledAuth = functions.https.onCall((uid)=>{
-    return admin.auth().updateUser(uid,{
+exports.disabledAuth = functions.https.onCall((uid) => {
+    return admin.auth().updateUser(uid, {
         disabled: true
-    }).then( async (userRecord)=>{
+    }).then(async (userRecord) => {
         try {
             let updateUser = await admin.firestore().collection('users').doc(uid).update({
                 disabled: true
@@ -28,26 +28,26 @@ exports.disabledAuth = functions.https.onCall((uid)=>{
                 success: true,
                 msg: `${userRecord.toJSON().email} is Disabled`
             }
-        }catch(e){
+        } catch (e) {
             return {
                 success: false,
-                cc:'Error while updating user collections',
+                cc: 'Error while updating user collections',
                 msg: e
             }
         }
-    }).catch(e=>{
+    }).catch(e => {
         return {
             success: false,
-            cc:'Error while disabling user auth',
+            cc: 'Error while disabling user auth',
             msg: e
         }
     })
 })
 
-exports.enabledAuth = functions.https.onCall((uid)=>{
-    return admin.auth().updateUser(uid,{
+exports.enabledAuth = functions.https.onCall((uid) => {
+    return admin.auth().updateUser(uid, {
         disabled: false
-    }).then(async (userRecord)=>{
+    }).then(async (userRecord) => {
         try {
             let updateUser = await admin.firestore().collection('users').doc(uid).update({
                 disabled: false
@@ -56,115 +56,115 @@ exports.enabledAuth = functions.https.onCall((uid)=>{
                 success: true,
                 msg: `${userRecord.toJSON().email} is Enabled`
             }
-        }catch(e){
+        } catch (e) {
             return {
                 success: false,
-                cc:'Error while updating user collections',
+                cc: 'Error while updating user collections',
                 msg: e
             }
         }
-    }).catch(e=>{
+    }).catch(e => {
         return {
             success: false,
-            cc:'Error while disabling user auth',
+            cc: 'Error while disabling user auth',
             msg: e
         }
     })
 })
 
-exports.removeAuth = functions.https.onCall((data)=>{
+exports.removeAuth = functions.https.onCall((data) => {
     console.log(data)
     const uid = data.uid
     const name = data.name
     const communityEmail = data.communityEmail
     const communityName = data.communityName
-    let email =''
+    let email = ''
 
     admin.auth().getUser(uid)
-    .then(function(userRecord) {
-        console.log('Successfully fetched user data:', userRecord.toJSON());
-        email = userRecord.toJSON().email
-    })
-    .catch(function(error) {
-        console.log('Error fetching user data:', error);
-    });
+        .then(function (userRecord) {
+            console.log('Successfully fetched user data:', userRecord.toJSON());
+            email = userRecord.toJSON().email
+        })
+        .catch(function (error) {
+            console.log('Error fetching user data:', error);
+        });
 
     return admin.auth().deleteUser(uid)
-    .then(()=>{
-        console.log('Successfully deleted user');
-        return admin.firestore().collection('users').doc(uid).delete().then(async ()=>{
-            let maildata = await sendGoodbyeEmail(email, name, communityName, communityEmail);
-            return {
-                success: true,
-                mailstatus: maildata,
-                msg:'Successfully deleted user'
-            }
+        .then(() => {
+            console.log('Successfully deleted user');
+            return admin.firestore().collection('users').doc(uid).delete().then(async () => {
+                let maildata = await sendGoodbyeEmail(email, name, communityName, communityEmail);
+                return {
+                    success: true,
+                    mailstatus: maildata,
+                    msg: 'Successfully deleted user'
+                }
+            })
         })
-    })
-    .catch((error)=>{
-        console.log('Error deleting user:', error);
-        return {
-            success: false,
-            msg:error
-        }
-    });
+        .catch((error) => {
+            console.log('Error deleting user:', error);
+            return {
+                success: false,
+                msg: error
+            }
+        });
 })
 
-exports.createAuthUser = functions.https.onCall(async(data)=>{
+exports.createAuthUser = functions.https.onCall(async (data) => {
     console.log(data)
-    const email = data.email 
+    const email = data.email
     const password = generateP()
     const name = data.name
     const userType = data.userType
     const communityEmail = data.communityEmail
     const communityName = data.communityName
     return admin.auth().createUser({
-        email: email,
-        password: password
-    })
-    .then((userRecord)=> {
-        console.log('Successfully created new user:', userRecord.uid);
-        return admin.firestore().collection('users').doc(userRecord.uid).set({
-            uid:userRecord.uid,
-            userType: userType,
-            id:data.id,
-            disabled: false 
-        }).then(async ()=>{
-            console.log('User Created with uid: '+ userRecord.uid)
-            let maildata = await sendWelcomeEmail(email, name, password, communityEmail, communityName);
-            return admin.firestore().collection('team').doc(data.id).update({
-                uid:userRecord.uid
-            }).then(()=>{
-                return {
-                    success: true,
-                    emailstatus: maildata,
-                    msg: 'Successfully created new user', 
-                    uid: userRecord.uid,
-                    docid: data.id
-                }
-            }).catch(e=>{
+            email: email,
+            password: password
+        })
+        .then((userRecord) => {
+            console.log('Successfully created new user:', userRecord.uid);
+            return admin.firestore().collection('users').doc(userRecord.uid).set({
+                uid: userRecord.uid,
+                userType: userType,
+                id: data.id,
+                disabled: false
+            }).then(async () => {
+                console.log('User Created with uid: ' + userRecord.uid)
+                let maildata = await sendWelcomeEmail(email, name, password, communityEmail, communityName);
+                return admin.firestore().collection('team').doc(data.id).update({
+                    uid: userRecord.uid
+                }).then(() => {
+                    return {
+                        success: true,
+                        emailstatus: maildata,
+                        msg: 'Successfully created new user',
+                        uid: userRecord.uid,
+                        docid: data.id
+                    }
+                }).catch(e => {
+                    return {
+                        success: false,
+                        emailstatus: maildata,
+                        msg: 'Problem while updating team data',
+                        error: e
+                    }
+                })
+            }).catch(e => {
                 return {
                     success: false,
-                    emailstatus: maildata,
-                    msg: 'Problem while updating team data',
-                    error:e
+                    msg: 'Error in Creating User : ' + e,
                 }
             })
-        }).catch(e=>{
+
+        })
+        .catch((error) => {
+            console.log('Error creating new user:', error);
             return {
                 success: false,
-                msg: 'Error in Creating User : ' + e, 
+                msg: 'Error in Creating User : ' + error,
             }
-        })
-    
-    })
-    .catch((error)=> {
-        console.log('Error creating new user:', error);
-        return {
-            success: false,
-            msg: 'Error in Creating User : ' + error, 
-        }
-    });
+        });
 })
 
 async function sendWelcomeEmail(email, displayName, pass, communityEmail, communityName) {
@@ -185,20 +185,20 @@ async function sendWelcomeEmail(email, displayName, pass, communityEmail, commun
         <p>Regards</p>
         <p>Team ${communityName}</p>
     `;
-    try{
+    try {
         let data = await mailTransport.sendMail(mailOptions);
         return {
             success: true,
             msg: `Mail Sent to ${email}`
         }
-    }catch(e){
+    } catch (e) {
         console.log(e);
         return {
             success: false,
             msg: `Error ${e}`
         }
     }
-    
+
 }
 
 async function sendGoodbyeEmail(email, name, communityName, communityEmail) {
@@ -217,31 +217,30 @@ async function sendGoodbyeEmail(email, name, communityName, communityEmail) {
         <p>Regards</p>
         <p>Team ${communityName}</p>
     `;
-    try{
+    try {
         let data = await mailTransport.sendMail(mailOptions);
         return {
             success: true,
             msg: `Mail Sent to ${email}`
         };
-    }catch(e){
+    } catch (e) {
         console.log(e);
         return {
             success: false,
             msg: `Error ${e}`
         };
     }
-    
+
 }
 
-function generateP() { 
-    var pass = ''; 
-    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +  
-            'abcdefghijklmnopqrstuvwxyz0123456789@#$'; 
-    for (let i = 1; i <= 8; i++) { 
-        var char = Math.floor(Math.random() 
-                    * str.length + 1); 
+function generateP() {
+    var pass = '';
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+        'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+    for (let i = 1; i <= 8; i++) {
+        var char = Math.floor(Math.random() * str.length + 1);
 
-        pass += str.charAt(char) 
-    } 
-    return pass; 
-} 
+        pass += str.charAt(char);
+    }
+    return pass;
+}
