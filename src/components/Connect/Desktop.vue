1935fe9fe364ffd432c22bac0d629015d51ae56f
Vrijraj Singh <vrijraj2396@gmail.com>
2020-06-06 23:03:13 +0530

    Chat ui added

--- src/components/Connect/Desktop.vue
+++ src/components/Connect/Desktop.vue
@@ -1,28 +1,55 @@
 <template>
   <v-container fluid class="my-0 py-0">
-    <v-row class="ma-0 pa-0">
-      <v-col cols="3" class="ma-0 pa-0 scrollable" style="border:1px solid #e0e0e0">
-        <!-- {{users}} -->
-        <v-list-item v-for="item in users" :key="item.id">
-          <v-list-item-avatar>
-            <v-img
-              :src="item.image.length > 0? item.image: require('@/assets/img/default_avatar.jpg')"
-            ></v-img>
-          </v-list-item-avatar>
-          <v-list-item-content>
-            <v-list-item-title class="google-font" v-html="item.name"></v-list-item-title>
-            <v-list-item-subtitle class="google-font" v-html="item.id"></v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
+    <v-row class="ma-0 pa-0" style="">
+      <v-col
+        cols="3"
+        class="ma-0 pa-0 pb-0"
+        style="border:1px solid #e0e0e0;"
+      >
+        <v-container
+          id="scroll-target"
+          style="max-height: 93vh"
+          class="overflow-y-auto"
+        >
+          <v-row
+            v-scroll:#scroll-target="onScroll"
+            style="height: 1000px"
+          >
+            <v-col>
+              <v-list-item v-for="item in users" :key="item.id">
+                <v-list-item-avatar>
+                  <v-img
+                    :src="
+                      item.image.length > 0
+                        ? item.image
+                        : require('@/assets/img/default_avatar.jpg')
+                    "
+                  ></v-img>
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title
+                    class="google-font"
+                    v-html="item.name"
+                  ></v-list-item-title>
+                  <v-list-item-subtitle
+                    class="google-font"
+                    v-html="item.id"
+                  ></v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-col>
-      <v-col cols="9" class="ma-0 pa-0 grey">
+      <v-col cols="9" class="ma-0 pa-0">
         <Chat />
         <v-text-field
-            label="Type here..."
-            class="mx-3 my-0 rem mt-3"
-            solo
-            v-model="content"
-            @keyup.enter="enterData"
+          label="Type here..."
+          hide-details
+          class="mx-3 my-0 mt-5 mb-0 pb-3"
+          solo
+          v-model="content"
+          @keyup.enter="enterData"
         ></v-text-field>
         <!-- style="position:fixed; bottom:0;right: 0;" -->
         <!-- <v-textarea
@@ -47,31 +74,34 @@ import { mapState } from "vuex";
 export default {
   name: "Desktop-Connect",
   components: {
-    Chat: () => import("./Chat")
+    Chat: () => import("./Chat"),
   },
   data: () => ({
     users: [],
     isLoadingUsers: [],
-    content: ""
+    content: "",
+    offsetTop: 0,
   }),
   computed: { ...mapState(["userDetails"]) },
   mounted() {
     this.getUsers();
   },
   methods: {
+    onScroll (e) {
+        this.offsetTop = e.target.scrollTop
+    },
     getUsers() {
       this.isLoadingUsers = true;
       UserService.getAllUsers()
-        .then(res => {
+        .then((res) => {
           console.log(res);
           if (res.success) {
             this.users = res.data;
-            this.users = this.users.filter(user=>user.disabled== false)
-            // console.log(this.users)
+            this.users = this.users.filter((user) => user.disabled == false);
           }
           this.isLoadingUsers = false;
         })
-        .catch(e => {
+        .catch((e) => {
           console.log(e);
         });
     },
@@ -82,23 +112,26 @@ export default {
           .add({
             message: this.content,
             name: this.userDetails.id,
-            timestamp: Date.now()
+            timestamp: Date.now(),
           })
-          .catch(err => {
+          .catch((err) => {
             console.log(err);
           });
         this.content = null;
       } else {
         // this.errorText = "A message must be entered!";
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
 .scrollable {
   overflow-y: auto;
-  height: 90vh;
+  height: 100%;
+  /* position: absolute; */
+  top: 0;
+  bottom: 0;
 }
 </style>
